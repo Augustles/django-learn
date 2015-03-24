911856d251ae36b38b83ddec7f903149c705cd8b
TToo <TToo@TToos-Mac.local>
2015-03-24 21:11:58 +0800

    改配

--- account/models.py
+++ account/models.py
@@ -6,5 +6,6 @@ class User(models.Model): # 创建User表
     username = models.CharField(max_length = 50) # username fields
     password = models.CharField(max_length = 50) # Create password fields
     email = models.EmailField() # Create email fields
+    headImg = models.FileField(upload_to='./upload')
     def __unicode__(self):
         return self.username
