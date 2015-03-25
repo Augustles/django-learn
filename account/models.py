99f3808ecd8314eda57b4001027116cdedfa39e5
Augustles <1927064778@qq.com>
2015-03-25 21:20:38 +0800

    添加注释

--- account/models.py
+++ account/models.py
@@ -6,6 +6,6 @@ class User(models.Model): # 创建User表
     username = models.CharField(max_length = 50) # username fields
     password = models.CharField(max_length = 50) # Create password fields
     email = models.EmailField() # Create email fields
-    headImg = models.FileField(upload_to='./upload')
+    headImg = models.FileField(upload_to='./upload') # 创建headImg文件上传路径字段
     def __unicode__(self):
-        return self.username
+        return self.username # 修正显示object问题
