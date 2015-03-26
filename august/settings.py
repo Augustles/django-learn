f1586c72b5ff8c76528e74c7c78f0d2b690ff065
augustles <1927064778@qq.com>
2015-03-26 08:59:21 +0800

    test

--- august/settings.py
+++ august/settings.py
@@ -110,7 +110,7 @@ ROOT_URLCONF = 'august.urls'
 WSGI_APPLICATION = 'august.wsgi.application'
 
 TEMPLATE_DIRS = (
-    '/Users/TToo/django/template'
+    '/home/ttoo/sites/django/template'
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
